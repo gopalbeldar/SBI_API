42475cb7b41ffd25d4fb9cbc05772f92740bfcb2
gopal <gopalbeldar@gmail.com>
2023-03-29 09:02:48 +0530

    Done some changes in file

--- Test.java
+++ Test.java
@@ -4,9 +4,10 @@ public static void main(String args[]){
 
   String s='Gopal';
 System.out.println(s);
+int a=10,b=20;
+int result;
+System.out.println(result);
 
-int i=10;
-System.out.println("Hello");
 
 }
 }
